82ec8d4a40a63f9a44c717d7dc485dadc552ca2d
Adam Furman <adamfurman7@gmail.com>
2022-03-11 18:58:31 -0600

    Added copy to clipboard functionality.

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ let characters = [
     "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"
 ]
 // declare variable for password length
-var passwordLength = document.querySelector(".output").value
+var passwordLength = document.querySelector(".slider").value
+document.querySelector(".slider").addEventListener("input", populate)
 // declare variables of textContent for password button
 let passwordEl1 = document.querySelector(".pwBtn1")
 let passwordEl2 = document.querySelector(".pwBtn2")
 let passwordEl3 = document.querySelector(".pwBtn3")
 let passwordEl4 = document.querySelector(".pwBtn4")
+
+let copy1 = document.querySelector("#copy1")
+let copy2 = document.querySelector("#copy2")
+let copy3 = document.querySelector("#copy3")
+let copy4 = document.querySelector("#copy4")
 
 // Generate a random password
 function generatePassword() {
@@ -26,17 +32,40 @@ function generatePassword() {
 // changes password text color from placeholder to a more legible color
 function changeTextColor() {
     window.getComputedStyle(document.documentElement).getPropertyValue("--text-color")
-    document.querySelector(".generate-wrap").addEventListener("click", () => {
         document.documentElement.style.setProperty("--text-color", "#10B981")
-    })
 }
 
 // populate password fields with password and change text color
 function populate() {
-    changeTextColor()
+    passwordLength = document.querySelector(".slider").value
+    changeTextColor();
     passwordEl1.textContent = generatePassword()
     passwordEl2.textContent = generatePassword()
     passwordEl3.textContent = generatePassword()
     passwordEl4.textContent = generatePassword()
 }
 
+// Copy to clipboard & notify user of action
+copy1.addEventListener('click', async () => {
+    await navigator.clipboard.writeText(passwordEl1.textContent)
+    alert("Copied! 📋")
+    console.log("Copied! 📋")
+})
+
+copy2.addEventListener('click', async () => {
+    await navigator.clipboard.writeText(passwordEl2.textContent)
+    alert("Copied! 📋")
+    console.log("Copied! 📋")
+})
+
+copy3.addEventListener('click', async () => {
+    await navigator.clipboard.writeText(passwordEl3.textContent)
+    alert("Copied! 📋")
+    console.log("Copied! 📋")
+})
+
+copy4.addEventListener('click', async () => {
+    await navigator.clipboard.writeText(passwordEl4.textContent)
+    alert("Copied! 📋")
+    console.log("Copied! 📋")
+})
